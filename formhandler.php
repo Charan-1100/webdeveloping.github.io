54df931f6b29d68f32c6381ede2699c876249b6c
Charan-1100 <107052321+Charan-1100@users.noreply.github.com>
2022-06-17 12:56:57 +0530

    Update formhandler.php

--- formhandler.php
+++ formhandler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'exc@yourwebsite.com';
+$email_from = 'info@webdeveloping.github.io';
 
 $email_subject = 'New Form Submission';
 
