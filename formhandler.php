298710a3663c537638b808385c0195943f54ab56
Charan-1100 <chinnukstar143@Gmail.com>
2022-06-17 12:45:03 +0530

    web developing

---
+++ formhandler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'exc@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "User Email: $subject.\n".
+              "User Email: $message.\n";
+
+$to = 'saicharan3168@gmail.com';
+
+$headers = "From: $email_from \r\n"
+
+$headers = "Reply-To: $visitor_email \r\n"
+
+mail($to,$email_subjectm,$email_body,$headers);
+header("Location: contact.html");
+
+?>
